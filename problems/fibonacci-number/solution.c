3c0fa5160afb16fac6de2a74252cea36c9310f98
LeetCode Backfill <you@example.com>
2026-02-06 17:56:35 +0530

    Solved: Fibonacci Number

---
+++ problems/fibonacci-number/solution.c
@@ -0,0 +1,13 @@
+
+
+int fib(int n){
+    if (n==0) return 0;
+    if (n==1) return 1;
+    int n1=0,n2=1,n3=0;
+    for (int i=1;i<n;i++){
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+    }
+    return n3;
+}
