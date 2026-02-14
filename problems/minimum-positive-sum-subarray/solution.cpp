4b40f8a5c4c6902ca3681ea4c30069db86793dae
LeetCode Backfill <you@example.com>
2026-02-14 11:43:12 +0530

    Solved: Minimum Positive Sum Subarray

---
+++ problems/minimum-positive-sum-subarray/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumSumSubarray(vector<int>& nums,int l,int r){
+        int n=nums.size();
+        int ans=INT_MAX;
+
+        for(int i=0;i<n;i++){
+            int sum=0;
+
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                int len=j-i+1;
+
+                if(len>=l && len<=r && sum>0){
+                    ans=min(ans,sum);
+                }
+            }
+        }
+
+        return(ans==INT_MAX?-1:ans);
+    }
+};
