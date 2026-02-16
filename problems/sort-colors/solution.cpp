66100a5662765a57b59435b613d724fe5fbf4ed6
LeetCode Backfill <you@example.com>
2026-02-16 14:37:54 +0530

    Solved: Sort Colors

---
+++ problems/sort-colors/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0,m=0,r=nums.size()-1;
+        while (m<=r){
+            if (nums[m]==2){
+                int temp=nums[r];
+                nums[r]=nums[m];
+                nums[m]=temp;
+                r--;
+            }
+            else if (nums[m]==1){
+                m++;
+            }
+            else if (nums[m]==0){
+                int temp=nums[l];
+                nums[l]=nums[m];
+                nums[m]=temp;
+                l++;
+                m++;
+            }
+        }
+    }
+};
