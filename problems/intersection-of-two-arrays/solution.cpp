4b2337a1227bd0cfb6bec08ac570e782e5418967
LeetCode Backfill <you@example.com>
2026-02-17 19:09:05 +0530

    Solved: Intersection of Two Arrays

---
+++ problems/intersection-of-two-arrays/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> mp1;
+        vector<int> nums3;
+        for (int i=0;i<nums1.size();i++){
+            mp1[nums1[i]]++;
+        }
+        int index=0;
+        for (int i=0;i<nums2.size();i++){
+            if (mp1[nums2[i]]>=1){
+                nums3.push_back(nums2[i]);
+                mp1[nums2[i]]=0;
+                index++;
+            }
+        }
+        return nums3;
+        
+    }
+};
