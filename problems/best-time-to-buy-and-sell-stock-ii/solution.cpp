652aa1da34dc26bce4efd8a972efdbe9899a8502
LeetCode Backfill <you@example.com>
2026-02-20 18:46:09 +0530

    Solved: Best Time to Buy and Sell Stock II

---
+++ problems/best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxprice=0;
+        for (int i=1;i<prices.size();i++){
+            if (prices[i-1]<prices[i]){
+                maxprice+=(prices[i]-prices[i-1]);
+            }
+        }
+        return maxprice;
+    }
+};
