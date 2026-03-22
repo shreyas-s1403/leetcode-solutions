3329e7c02e72fc56b3f657845f1ce7bb6079c557
LeetCode Backfill <you@example.com>
2026-03-22 20:20:46 +0530

    Solved: Determine Whether Matrix Can Be Obtained By Rotation

---
+++ problems/determine-whether-matrix-can-be-obtained-by-rotation/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+        vector<vector<int>> temp(n, vector<int>(n));
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                temp[j][n - 1 - i] = mat[i][j];
+            }
+        }
+        
+        mat = temp;
+    }
+    
+    bool isEqual(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        return mat == target;
+    }
+    
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        for(int i = 0; i < 4; i++) {
+            if(isEqual(mat, target)) return true;
+            rotate(mat);
+        }
+        return false;
+    }
+};
