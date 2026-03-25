42cd14826de4ac7abcc2108dc0dec3de7eb5817e
LeetCode Backfill <you@example.com>
2026-03-25 20:43:21 +0530

    Solved: Jump Game II

---
+++ problems/jump-game-ii/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int current_end = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == current_end) {
+                jumps++;
+                current_end = farthest;
+            }
+        }
+
+        return jumps;
+    }
+};
