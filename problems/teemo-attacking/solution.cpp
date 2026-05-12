a216ec5d87d170ef17aa7a75dbc4fa80bfc4afa5
LeetCode Backfill <you@example.com>
2026-05-12 18:54:26 +0530

    Solved: Teemo Attacking

---
+++ problems/teemo-attacking/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int total=0;
+        for (int i=1;i<timeSeries.size();i++){
+            total+=min(duration,timeSeries[i]-timeSeries[i-1]);
+        }
+        return total+duration;
+    }
+};
