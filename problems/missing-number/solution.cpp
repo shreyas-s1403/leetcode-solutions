fc66c307e891dfa9e9a411863716f0386a6da083
LeetCode Backfill <you@example.com>
2026-05-22 16:45:02 +0530

    Solved: Missing Number

---
+++ problems/missing-number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        if (nums[0]!=0) return 0;
+        for (int i=1;i<nums.size();i++){
+            if (nums[i-1]+1!=nums[i]){
+                return nums[i-1]+1;
+            }
+        }
+        return nums[nums.size()-1]+1;
+    }
+};
