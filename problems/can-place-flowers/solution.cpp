61a2540e7c7c2a82e2f4ac38869880668c2a8dd1
LeetCode Backfill <you@example.com>
2026-07-19 20:57:16 +0530

    Solved: Can Place Flowers

---
+++ problems/can-place-flowers/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count=0;
+        if (n==0) return true;
+        for (int i=0;i<flowerbed.size();i++){
+            if ((i==0||flowerbed[i-1]==0) && (i==flowerbed.size()-1||flowerbed[i+1]==0)){
+                if (flowerbed[i]==0){
+                    count++;
+                    flowerbed[i]=1;
+                    if (count>=n) return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
