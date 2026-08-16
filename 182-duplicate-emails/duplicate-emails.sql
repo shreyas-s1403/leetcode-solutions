4a5239f790e24c66fd7d0847cfac863bcfc2aac3
Shreyas S <shreyas.sredar@gmail.com>
2026-08-16 09:05:25 +0530

    Time: 465 ms (21.49%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select email as Email from Person group by email having count(*)>=2; 
+select email as Email from Person group by email having count(email)>=2; 
