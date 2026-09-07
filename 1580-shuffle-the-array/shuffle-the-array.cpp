4f65e9cfeddef5afa14ee1211cb3126b4f56120d
Shreyas S <shreyas.sredar@gmail.com>
2026-09-07 13:19:18 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (19.35%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>ans;
+        int MOD=1024;
         for (int i=0;i<n;i++){
-            ans.push_back(nums[i]);
-            ans.push_back(nums[i+n]);
+            nums[i]+=(nums[i+n]%MOD)*MOD;
         }
-        return ans;
+        for (int i=n-1;i>=0;i--){
+            int x=nums[i]%MOD;
+            int y=nums[i]/MOD;
+        nums[2*i+1]=y;
+        nums[2*i]=x;}
+        return nums;
     }
 };
