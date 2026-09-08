b201fde0c3d26f0609c6a018667caa093a24d196
Shreyas S <shreyas.sredar@gmail.com>
2026-09-08 11:31:09 +0530

    Time: 52 ms (33.44%) | Memory: 29.2 MB (46.08%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        map<int, int> index;
-        set<vector<int>> unique;
-        vector<vector<int>> ans;
-
-        for (int i = 0; i < nums.size(); i++)
-            index[nums[i]] = i;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int target = nums[i];
-
-            for (int j = i + 1; j < nums.size(); j++) {
-                int third = -(target + nums[j]);
-
-                if (index.find(third) != index.end()) {
-                    int k = index.find(third)->second;
-
-                    if (k > j) {
-                        vector<int> triplet = {target, nums[j], third};
-                        sort(triplet.begin(), triplet.end());
-                        unique.insert(triplet);
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        for (int i=0;i<nums.size();i++){
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            int left=i+1;
+            int right=nums.size()-1;
+            while (left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if (sum==0){
+                    ans.push_back({nums[left],nums[i],nums[right]});
+                    while (left<right && nums[left]==nums[left+1]){
+                        left++;
                     }
+                    while (left<right && nums[right]==nums[right-1]){
+                        right--;
+                    }
+                    left++;
+                    right--;
                 }
+                else if (sum<0) left++;
+                else right--;
             }
         }
-
-        for (auto x : unique)
-            ans.push_back(x);
-
         return ans;
     }
 };
