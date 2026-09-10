66db43a3d133ced910fc561b9a05ab321fd5b81a
Shreyas S <shreyas.sredar@gmail.com>
2026-09-10 11:36:39 +0530

    Time: 3 ms (66.12%) | Memory: 10 MB (46.21%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         if (s1.size()>s2.size()) return false;
-        map<char,int>m1,m2;
+        vector<int>v1(26,0);
+        vector<int>v2(26,0);
         for (int i=0;i<s1.size();i++){
-            m1[s1[i]]++;
-            m2[s2[i]]++;
+            v1[s1[i]-'a']++;
+            v2[s2[i]-'a']++;
         }
-        if (m1==m2) return true;
+        if (v1==v2) return true;
         for (int i=s1.size();i<s2.size();i++){
-            m2[s2[i]]++;
-            m2[s2[i-s1.size()]]--;
-            if (m2[s2[i-s1.size()]]==0){
-                m2.erase(s2[i-s1.size()]);
-            }
-            if (m1==m2) return true;
+            v2[s2[i]-'a']++;
+            v2[s2[i-s1.size()]-'a']--;
+            
+            if (v1==v2) return true;
         }
         return false;
     }
