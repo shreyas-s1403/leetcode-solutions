dc72e32192df43fa8875b90fc830b33700467467
Shreyas S <shreyas.sredar@gmail.com>
2026-09-15 11:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (62.19%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> ans;
-
+        //using array marking
         for (int i = 0; i < nums.size(); i++) {
             int index = abs(nums[i]) - 1;
 
