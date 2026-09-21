347ceaa4587d9239d6074ff54980eebd6923d6c4
Shreyas S <shreyas.sredar@gmail.com>
2026-09-21 19:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (97.95%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int freq[26]={0};
+        for (char ch:magazine) freq[ch-'a']++;
+        for (char ch:ransomNote){
+            freq[ch-'a']--;
+            if (freq[ch-'a']<0) return false;
+        } 
+        return true;
+    }
+};
